86b69de4f477d3817cd887e639acaa8f829e597d
ElliePei <ellie.pei@everbridge.com>
2018-08-06 11:24:48 +0800

    LF

--- com.everbridge.dev4/com.src.java/NewTestFile.java
+++ com.everbridge.dev4/com.src.java/NewTestFile.java
@@ -1,1 +1,6 @@
-public class NewTestFile {    public static void main(String[] args) {        System.out.print("This is a new item");        System.out.print("This is a new dfdfd");    }}
+public class NewTestFile {
+    public static void main(String[] args) {
+        System.out.print("This is a new item");
+        System.out.print("This is a new dfdfd");
+    }
+}
