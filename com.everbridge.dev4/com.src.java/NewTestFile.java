e030685e7d89cfaf23a989f60b691925f4c7a24f
ElliePei <ellie.pei@everbridge.com>
2018-09-27 16:23:51 +0800

    test

--- com.everbridge.dev4/com.src.java/NewTestFile.java
+++ com.everbridge.dev4/com.src.java/NewTestFile.java
@@ -2,5 +2,6 @@ public class NewTestFile {
     public static void main(String[] args) {
         System.out.print("This is a new item");
         System.out.print("This is a new dfdfd");
+        System.out.print("this is a new commit");
     }
 }
